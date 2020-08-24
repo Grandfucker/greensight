0913d875c0aeb526d0ff3f4a7e10cdcf5d11aa33
[grandfucker] <vcheplev42@gmail.com>
2020-08-24 14:34:54 +0300

    finished

--- script.js
+++ script.js
@@ -55,22 +55,90 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     initMap();
 
-
-    const firstAddress = document.querySelector('.first__address'),
-        secondAddress = document.querySelector('.second__address'),
+    const mapFirstAddress = document.querySelector('.map__first__address'),
+        mapSecondAddress = document.querySelector('.map__second__address'),
         BOUNCE = google.maps.Animation.BOUNCE;
-
-
-    firstAddress.addEventListener('click', (e) => {
-        if (e.target && e.target.classList.contains('first__address')) {
-            marker2.setAnimation(BOUNCE);
-            marker1.setAnimation(null);
-        }
+        
+    mapFirstAddress.addEventListener('click', (e) => {
+        mapFirstAddress.style.color = ('#215BF0');
+        mapSecondAddress.style.color = ('#222222');
+        marker2.setAnimation(BOUNCE);
+        marker1.setAnimation(null);
     });
 
-    secondAddress.addEventListener('click', (e) => {
-        console.log(e.target);
+    mapSecondAddress.addEventListener('click', (e) => {
+        mapFirstAddress.style.color = ('#222222');
+        mapSecondAddress.style.color = ('#215BF0');
         marker1.setAnimation(BOUNCE);
-        marker2.setAnimation(null)
+        marker2.setAnimation(null);
     });
+
+    let inputs = document.querySelectorAll('input[data-rule');
+
+    for (let input of inputs) {
+        input.addEventListener('blur', function (e) {
+            let rule = this.dataset.rule,
+                value = this.value,
+                check,
+                alert = document.querySelectorAll('.alert'),
+                alertText = document.querySelectorAll('.alert__text');
+
+
+            for (let i = 0; i < alert.length; i++) {
+                switch (rule) {
+                    case 'name':
+                        check = /^[?!,.а-яА-ЯёЁ\s]+$/.test(value);
+                        if (check) {
+                            alert[0].style.display = 'none';
+                            alertText[0].style.display = 'none';
+                        } else {
+                            alert[0].style.display = 'block';
+                            alertText[0].style.display = 'block';
+                        }
+                        break;
+                    case 'number':
+                        check = /^\d+$/.test(value);
+                        if (check) {
+                            alert[1].style.display = 'none';
+                            alertText[1].style.display = 'none';
+                        } else {
+                            alert[1].style.display = 'block';
+                            alertText[1].style.display = 'block';
+                        }
+                        break;
+                    case 'address':
+                        check = /^[?!,.а-яА-ЯёЁ0-9\s]+$/.test(value);
+                        if (check) {
+                            alert[2].style.display = 'none';
+                            alertText[2].style.display = 'none';
+                        } else {
+                            alert[2].style.display = 'block';
+                            alertText[2].style.display = 'block';
+                        }
+                        break;
+
+                    case 'comments':
+                        check = /^[?!,.а-яА-ЯёЁ0-9\s]+$/.test(value);
+                        if (check) {
+                            alert[3].style.display = 'none';
+                            alertText[3].style.display = 'none';
+                        } else {
+                            alert[3].style.display = 'block';
+                            alertText[3].style.display = 'block';
+                        }
+                        break;
+                }
+
+            }
+        });
+    }
+
+    const element = document.querySelector('#userphone');
+
+    const maskOptions = {
+
+        mask: '+{7}(000)000-00-00',
+    };
+    const mask = new IMask(element, maskOptions);
+
 });
